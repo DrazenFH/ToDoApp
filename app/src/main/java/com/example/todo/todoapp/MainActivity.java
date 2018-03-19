ffcb84cf4c7524c95f44b807630ba414ba425697
DrazenFH <drazen.lukic@students.fhv.at>
2018-03-19 12:31:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/todo/todoapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.todo.todoapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private int mTheme = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        mTheme = R.style.CustomStyle_LightTheme;
+
+        this.setTheme(mTheme);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.aboutMeMenuItem:
+                Intent i = new Intent(this, AboutActivity.class);
+                startActivity(i);
+                return true;
+
+            case R.id.preferences:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/todo/todoapp/SettingsActivity.java
@@ -0,0 +1,8 @@
+package com.example.todo.todoapp;
+
+/**
+ * Created by dl_asus on 19.03.2018.
+ */
+
+class SettingsActivity {
+}
